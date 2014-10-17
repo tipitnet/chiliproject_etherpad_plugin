9f309962fd957fdffd51aadc69232697e26cf8d9
NicoPaez <nicopaez@gmail.com>
2014-10-17 11:55:54 -0300

    Support for multiple chili instances added

--- app/controllers/pads_controller.rb
+++ app/controllers/pads_controller.rb
@@ -15,12 +15,32 @@ class PadsController < ApplicationController
     ENV['PAD_DOMAIN']
   end
 
+  def pad_client_id
+    ENV['PAD_CLIENT_ID']
+  end
+
   def index
     @pad_host = self.pad_host
     session[:ep_sessions] = {} if session[:ep_sessions].nil?
     ether = EtherpadLite.connect(@pad_host, self.pad_key)
-    group = ether.group(@project.identifier)
+    group = ether.group(self.project_uid(@project))
     @pads = group.pads
+  end
+
+  def project_uid(project)
+    if self.pad_client_id.nil? || self.pad_client_id.empty?
+      return project.identifier
+    else
+      return "#{self.pad_client_id}-#{project.identifier}"
+    end
+  end
+
+  def user_uid(user)
+    if self.pad_client_id.nil? || self.pad_client_id.empty?
+      return "chili_user_#{user.id}"
+    else
+      return "#{self.pad_client_id}_chili_user_#{user.id}"
+    end
   end
 
   def new
@@ -29,7 +49,7 @@ class PadsController < ApplicationController
       pad_title = params[:pad][:title]
       session[:ep_sessions] = {} if session[:ep_sessions].nil?
       ether = EtherpadLite.connect(@pad_host, self.pad_key)
-      @group = ether.group(@project.identifier)
+      @group = ether.group(self.project_uid(@project))
       if @group.pads.select { |p| p.name == pad_title }.size == 0
         @pad = @group.pad(pad_title)
         redirect_to :action => :show, :pad_id => pad_title
@@ -44,7 +64,7 @@ class PadsController < ApplicationController
     @pad_host = self.pad_host
     session[:ep_sessions] = {} if session[:ep_sessions].nil?
     ether = EtherpadLite.connect(@pad_host, self.pad_key)
-    group = ether.group(@project.identifier)
+    group = ether.group(self.project_uid(@project))
     pad = group.pad(params[:pad_id])
     pad.delete
     redirect_to :action => :index, :project_id => @project
@@ -55,12 +75,12 @@ class PadsController < ApplicationController
     session[:ep_sessions] = {} if session[:ep_sessions].nil?
     ether = EtherpadLite.connect(@pad_host, self.pad_key)
     # Get the EtherpadLite Group and Pad by id
-    @group = ether.group(@project.identifier)
+    @group = ether.group(self.project_uid(@project))
     @pad = @group.pad(params[:pad_id])
     @pad_id = "#{@group.id}$#{@pad.name}"
     @user_name = User.current.name
     # Map the user to an EtherpadLite Author
-    author = ether.author("chili_user_#{User.current.id}", :name => User.current.name)
+    author = ether.author(self.user_uid(User.current), :name => User.current.name)
     # Get or create an hour-long session for this Author in this Group
     sess = session[:ep_sessions][@group.id] ? ether.get_session(session[:ep_sessions][@group.id]) : @group.create_session(author, 60)
     if sess.expired?
@@ -73,6 +93,7 @@ class PadsController < ApplicationController
   end
 
   private
+
   def find_project
     @project = Project.find(params[:project_id])
   rescue ActiveRecord::RecordNotFound
